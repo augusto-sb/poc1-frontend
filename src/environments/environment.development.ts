16ba6c8c876150f46c89d97dc28e0b15488bcdad
asaldivar <augustosaldivar@assertsolutions.com>
2025-04-30 11:18:15 -0300

    adds environment.development and backend client id for role checking

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  authUrl: "http://localhost:8080/auth",
+  authRealm: "poc",
+  authClientId: "public",
+  authBackendClientId: "private",
+  apiUrl: "http://localhost:8081/backend",
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   authUrl: "replace_authUrl",
   authRealm: "replace_authRealm",
   authClientId: "replace_authClientId",
+  authBackendClientId: "replace_authBackendClientId",
   apiUrl: "replace_apiUrl",
 };
