989aa2f16fb3a4b3ecc4e56de01888d3bb59fbc5
asaldivar <augustosaldivar@assertsolutions.com>
2025-04-30 12:33:49 -0300

    finishing routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,53 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot, Routes } from '@angular/router';
 
+//import { AppComponent } from './app.component';
 import { EntityComponent } from './entity/entity.component';
+import { DetailComponent } from './entity/detail/detail.component';
+import { CreateComponent } from './entity/create/create.component';
+import { UpdateComponent } from './entity/update/update.component';
+
+const resolvedUpdateTitle: ResolveFn<string> = (a:ActivatedRouteSnapshot,b:RouterStateSnapshot) => Promise.resolve(`Actualizar Entidad ${a.params['id']}`);
+const resolvedDetailTitle: ResolveFn<string> = (a:ActivatedRouteSnapshot,b:RouterStateSnapshot) => Promise.resolve(`Detalle Entidad ${a.params['id']}`);
 
 export const routes: Routes = [
+  /*{
+    path: '**',
+    title: 'Home',
+    component: AppComponent,
+  },*/
   {
     path: 'entity',
-    component: EntityComponent,
+    //title: 'Entidades',
+    //component: EntityComponent,
+    //canActivate: [],//TBI
+    children: [
+      {
+        path: '',
+        title: 'Entidades',
+        component: EntityComponent,
+        //canActivate: [],//TBI
+      },
+      {
+        path: 'create',
+        title: 'Crear Entidad',
+        component: CreateComponent,
+        //canActivate: [],//TBI
+      },
+      //dynamic routes after
+      {
+        path: 'update/:id',
+        //title: 'Actualizar Entidad :id',
+        title: resolvedUpdateTitle,
+        component: UpdateComponent,
+        //canActivate: [],//TBI
+      },
+      {
+        path: ':id',
+        //title: 'Detalle Entidad :id',
+        title: resolvedDetailTitle,
+        component: DetailComponent,
+        //canActivate: [],//TBI
+      },
+    ],
   },
-  /*{
-    path: 'entity/create',
-    component: EntityCreationComponent,
-  },*/
 ];
