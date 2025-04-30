afab8655525fc9c0f782c350792bffea32b02472
asaldivar <augustosaldivar@assertsolutions.com>
2025-04-30 11:51:12 -0300

    delete and page/size

--- src/app/entity/entity.component.ts
+++ src/app/entity/entity.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { KeycloakService } from '../keycloak.service';
 
@@ -19,19 +19,47 @@ type Entity = {
 export class EntityComponent {
   public entities: Entity[] = [];
   public amount: number = 0;
+  private page: number = 0;
+  private size: number = 2;
 
   constructor(
     private readonly httpClient: HttpClient,
     public ks: KeycloakService,
+    private route: ActivatedRoute,
   ){
+    route.queryParams.subscribe(params => {
+      if(params['page'] && !isNaN(params['page']) && Number(params['page']) >= 0){
+        this.page = Number(params['page']);
+      }
+      if(params['size'] && !isNaN(params['size']) && Number(params['size']) >= 1){
+        this.size = Number(params['size']);
+      }
+      //sort
+    });
+    let reqParams = new HttpParams();
+    reqParams = reqParams.set('size', this.size);
+    reqParams = reqParams.set('page', this.page);
+    //sort
 
-    let params = new HttpParams();
-    //params = params.set('size', '5');
-
-    httpClient.get<{Results: Entity[]; Count: number}>('/entities', { params }).subscribe(
-      body => {/*console.log(body);*/this.entities=body.Results;this.amount=body.Count;},
+    httpClient.get<{Results: Entity[]; Count: number}>('/entities', { params: reqParams }).subscribe(
+      body => {
+        this.entities = body.Results;
+        this.amount = body.Count;
+      },
       err=>{console.log('Error', err);},
       ()=>{/*console.log('Finish')*/;},
     );
+  }
+
+  public delete(id: number){
+    if(confirm('¿seguro?')){
+      this.httpClient.delete<any>(`/entities/${id}`).subscribe(
+        body => {
+          window.location.reload();
+        },
+        err=>{console.log('Error', err);},
+        ()=>{console.log('delete Finish');},
+      );
+    }
   }
 }
